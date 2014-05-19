ccf1aa79bfa21b4820286c50c6d9ffcd82ae1d94
Thomas Noe <jtruant@gmx.com>
2014-05-18 22:16:39 -0600

    flashy :)

--- app.py
+++ app.py
@@ -1,13 +1,21 @@
 #!/usr/bin/env python
 
-from flask import Flask, render_template
+from flask import Flask, render_template, flash
 app = Flask(__name__) 
+app.config.from_object(__name__)
+
+app.config.update(dict(
+    DEBUG=True,
+    SECRET_KEY='foo that bar',
+    HOST='0.0.0.0'
+))
 
 # app.route is called a decorator. don't worry about it too much right
 # now.
 @app.route('/')
 def hell_world():
     # do work
+    flash('UNDER DEVELOMENT')
     return render_template('app.html')
 
 # a few example pages
@@ -17,7 +25,8 @@ def hello():
 
 @app.route('/hello/<name>')
 def hello_name(name):
+    flash('testing: %s was here!' % name)
     return 'Hello %s' % name
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=True)
+    app.run()
