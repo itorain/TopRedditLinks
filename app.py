bf910c93f637e2944fab418570fc602944102efe
Thomas Noe <jtruant@gmx.com>
2014-05-18 20:49:40 -0600

    some sample website paths

--- app.py
+++ app.py
@@ -7,7 +7,16 @@ app = Flask(__name__)
 # now.
 @app.route('/')
 def hell_world():
-    return 'Hello World!'
+    return 'Top Reddit Links!'
+
+# a few example pages
+@app.route('/hello')
+def hello():
+    return 'Hello World'
+
+@app.route('/hello/<name>')
+def hello_name():
+    return 'Hello %s' % name
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+    app.run(host='0.0.0.0', debug=True)
